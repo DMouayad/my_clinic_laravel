160828a4113c30fe13d7e6a65c2c0607e8c317e3
Mouayad <muayad.perun@outlook.com>
2022-08-29 11:03:58 +0300

    update users table migration - add roleId

--- database/migrations/2022_08_26_000000_create_users_table.php
+++ database/migrations/2022_08_26_000000_create_users_table.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
+use App\Models\StaffInfo;
 
 return new class extends Migration
 {
@@ -17,7 +20,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->foreignIdFor(Role::class, 'role_id')->constrained();
+
             $table->timestamp('email_verified_at')->nullable();
+
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
