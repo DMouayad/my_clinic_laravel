e4dfbc743a636cfd7f87c795867ef342eca40b89
Mouayad <muayad.perun@outlook.com>
2022-09-05 17:07:14 +0300

    update Role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Exceptions\RoleNotFoundException;
 
 class Role extends Model
 {
@@ -21,5 +23,30 @@ class Role extends Model
     public function users()
     {
         return $this->hasMany(User::class);
+    }
+
+
+    /**
+     * Get Role by role slug.
+     * @param string $slug
+     * @return \App\Models\Role|null
+     **/
+    public static function findBySlug(string $slug)
+    {
+        return self::firstWhere('slug', $slug);
+    }
+    /**
+     * Get Role ID by role slug.
+     * @param string $slug
+     * @return int|null
+     * @throws \App\Exceptions\RoleNotFoundException
+     **/
+    public static function getIdBySlug(string $slug)
+    {
+        try {
+            return self::where('slug', $slug)->firstOrFail(['id'])->id;
+        } catch (ModelNotFoundException $e) {
+            throw new RoleNotFoundException($slug);
+        }
     }
 }
