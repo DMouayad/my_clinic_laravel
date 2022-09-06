1800e84c1d09d80b15984c490191818ac272545d
Mouayad <muayad.perun@outlook.com>
2022-09-06 15:51:50 +0300

    create UserService unit test suite

--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -2,15 +2,22 @@
 
 namespace Tests\Unit;
 
+
 use Illuminate\Http\JsonResponse;
+
+use App\Exceptions\EmailAlreadyRegisteredException;
+use App\Exceptions\UnauthorizedToDeleteUserException;
+use App\Exceptions\UserDoesntMatchHisStaffEmailException;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Database\Seeders\RoleSeeder;
 use Database\Seeders\StaffEmailSeeder;
 use Illuminate\Database\Seeder;
 use App\Services\UserService;
-use App\Traits\ProvidesApiJsonResponse;
-use Database\Seeders\Utils\ProvidesUserSeedingData;
+
+
+use Tests\Utils\Traits\ProvidesUsersForTesting;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,13 +37,11 @@ class DatabaseSeeder extends Seeder
 
 class UserServiceTest extends TestCase
 {
-    use  RefreshDatabase, ProvidesApiJsonResponse, ProvidesUserSeedingData;
+
+
+    use  RefreshDatabase, ProvidesUsersForTesting;
+
     private UserService $userService;
-    private $valid_user_credentials = [
-        'email' => 'admin@dentalClinic.com',
-        'name' => 'admin1',
-        'password' => 'clinic123',
-    ];
 
     protected $seed = true;
     /**
@@ -50,6 +55,7 @@ class UserServiceTest extends TestCase
         $this->userService = new UserService();
         parent::setUp();
     }
+
     public function test_user_creation_with_valid_credentials()
     {
         $response =   $this->userService->createNewUser(
@@ -60,5 +66,136 @@ class UserServiceTest extends TestCase
         var_dump($response->content());
         $this->asset;
         $this->assertEquals($response->status(), JsonResponse::HTTP_CREATED);
+    }
+
+    public function test_create_user_with_valid_credentials()
+    {
+        $user = $this->createAdminUser();
+        $this->assertModelExists($user);
+    }
+    public function test_creating_new_user_creates_staff_email_user()
+    {
+        $user = $this->createAdminUser();
+
+        $this->assertDatabaseHas(
+            'staff_email_user',
+            ['user_id' => $user->id, 'staff_email_id' => $user->staffEmail->id]
+        );
+    }
+    public function test_creating_user_with_existing_email_throws_exception()
+    {
+        $this->assertThrows(function () {
+            $this->userService->createNewUser(
+                $this->valid_admin_credentials['email'],
+                $this->valid_admin_credentials['name'],
+                $this->valid_admin_credentials['password']
+            );
+            $this->userService->createNewUser(
+                $this->valid_admin_credentials['email'],
+                $this->valid_admin_credentials['name'],
+                $this->valid_admin_credentials['password']
+            );
+        }, EmailAlreadyRegisteredException::class);
+    }
+    public function test_updating_user_role_and_email()
+    {
+        $new_email = 'updatedEmail@myclinic.com';
+        $user = $this->createAdminUser();
+        // user's staffEmail must be updated before updating the user
+        $user->staffEmail->update(['email' => $new_email, 'role_id' => 2]);
+        $updated = $this->userService->update($user,  2,  $new_email);
+        // assert user date was updated in the db
+        $this->assertDatabaseHas(
+            'users',
+            ['id' => $updated->id, 'role_id' => $updated->role_id, 'email' => $new_email]
+        );
+    }
+    public function test_updating_user_with_role_different_from_his_staffEmail_throws_exception()
+    {
+        $this->assertThrows(function () {
+
+            $new_role_id = 2;
+            $user = $this->createAdminUser();
+            $this->userService->update($user,  role_id: $new_role_id);
+        }, UserDoesntMatchHisStaffEmailException::class);
+    }
+    public function test_updating_user_with_email_different_from_his_staffEmail_throws_exception()
+    {
+        $this->assertThrows(function () {
+
+            $new_email = 'newEmail@myclinic.com';
+            $user = $this->createAdminUser();
+            $this->userService->update($user,  email: $new_email);
+        }, UserDoesntMatchHisStaffEmailException::class);
+    }
+    public function test_updating_user_email_throws_exception_if_already_exists()
+    {
+        $this->assertThrows(function () {
+            $user1 = $this->createDentistUser();
+            $user2 = $this->createSecretaryUser();
+            $this->userService->update($user1, email: $user2->email);
+        }, EmailAlreadyRegisteredException::class);
+    }
+    public function test_updating_user_email_revokes_his_tokens()
+    {
+        $new_email = 'updatedEmail@myclinic.com';
+        $user = $this->createAdminUser(store_token: true);
+        // assert user has one access token
+        $this->assertSame(1, $user->tokens->count());
+
+        $user->staffEmail->update(['email' => $new_email]);
+
+        // updates user's email and role_id
+        $updated = $this->userService->update($user, email: $new_email);
+        // get a fresh instance so tokens relationship will be updated
+        $updated = $updated->fresh();
+        // expects user's tokens have been deleted. 
+        $this->assertSame(0, $updated->tokens->count());
+    }
+    public function test_updating_user_role_revokes_his_tokens()
+    {
+        $user = $this->createAdminUser(store_token: true);
+
+        // assert user has one access token
+        $this->assertSame(1, $user->tokens->count());
+
+        $user->staffEmail->update(['role_id' => 2]);
+        // updates user's email and role_id
+        $updated = $this->userService->update($user, role_id: 2);
+        // get a fresh instance so tokens relationship will be updated
+        $updated = $updated->fresh();
+        // expects user's tokens have been deleted. 
+        $this->assertSame(0, $updated->tokens->count());
+    }
+
+    public function test_user_can_delete_his_account()
+    {
+        $user = $this->createDentistUser();
+        $this->userService->deleteUser($user, $user);
+        $this->assertModelMissing($user);
+    }
+    public function test_deleting_user_throws_exception_if_not_by_admin_or_himself()
+    {
+        $this->assertThrows(function () {
+            $user1 = $this->createDentistUser();
+            $user2 = $this->createSecretaryUser();
+            $this->userService->deleteUser($user1, $user2);
+        }, UnauthorizedToDeleteUserException::class);
+    }
+    public function test_admin_can_delete_other_users()
+    {
+        $admin = $this->createAdminUser(grant_token: true);
+
+        $user = $this->createSecretaryUser();
+        $this->userService->deleteUser($user, $admin);
+        $this->assertModelMissing($user);
+    }
+
+    public function test_deleting_user_will_delete_his_tokens()
+    {
+        $user = $this->createDentistUser(store_token: true);
+        $this->userService->deleteUser($user, $user);
+        $this->assertDatabaseMissing('personal_access_tokens', ['tokenable_id' => $user->id]);
+
     }
 }
---
+++ tests/Utils/Traits/ProvidesUsersForTesting.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Utils\Traits;
+
+use App\Models\User;
+use Database\Seeders\Utils\ProvidesUserSeedingData;
+use Laravel\Sanctum\Sanctum;
+
+trait ProvidesUsersForTesting
+{
+    use ProvidesUserSeedingData;
+    private $valid_admin_credentials = [
+        'email' => 'admin@myclinic.com',
+        'name' => 'admin1',
+        'password' => 'clinic123',
+    ];
+
+    /**
+     *
+     * @param boolean $grant_token
+     * @param boolean $store_token
+     * @return \App\Models\User
+     */
+    public function createAdminUser(bool $grant_token = false, bool $store_token = false): User
+    {
+        return $this->createUserByRole('admin', $grant_token, $store_token);
+    }
+    /**
+     *
+     * @param boolean $grant_token
+     * @param boolean $store_token
+     * @return \App\Models\User
+     */
+    public function createDentistUser(bool $grant_token = false, bool $store_token = false): User
+    {
+        return $this->createUserByRole('dentist', $grant_token, $store_token);
+    }
+    /**
+     *
+     * @param boolean $grant_token
+     * @param boolean $store_token
+     * @return User
+     */
+    public function createSecretaryUser(bool $grant_token = false, bool $store_token = false): User
+    {
+        return $this->createUserByRole('secretary', $grant_token, $store_token);
+    }
+    /**
+     * 
+     * @param string $role
+     * @param boolean $grant_token
+     * @param boolean $store_token
+     * @return \App\Models\User
+     */
+    private function createUserByRole(string $role, bool $grant_token = false, bool $store_token = false): User
+    {
+        $user = $this->userService->createNewUser(
+            $this->users_seeding_emails[$role],
+            'test ' . $role,
+            'password'
+        );
+        if ($grant_token) {
+            $this->giveToken($user, [$role]);
+        }
+        if ($store_token) {
+            $this->createToken($user, [$role]);
+        }
+        return $user;
+    }
+    /**
+     * creates a token for user which will work with tokenCan
+     * @param \App\Models\User $user
+     * @param array $abilities
+     * @return void
+     */
+    private function giveToken(User &$user, array $abilities = [])
+    {
+        Sanctum::actingAs($user,  $abilities);
+    }
+    /**
+     * Creates a token for user and store it in the db.
+     *
+     * @param \App\Models\User $user
+     * @param array $abilities
+     * @return void
+     */
+    private function createToken(User &$user, array $abilities = [])
+    {
+        $user->createToken('test_token', $abilities)->plainTextToken;
+    }
+}
