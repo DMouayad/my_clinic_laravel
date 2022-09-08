9a41b284e85f1f4d2cc6e6fc47d3a02e06112008
Mouayad <muayad.perun@outlook.com>
2022-09-08 16:02:01 +0300

    update authRoutes with names and middleware

--- routes/api/authRoutes.php
+++ routes/api/authRoutes.php
@@ -1,17 +1,21 @@
 <?php
 
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\VerificationController;
+use App\Http\Middleware\EnsureStaffEmailProvided;
 
-Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LogoutController::class, 'logout']);
+Route::post("login", [LoginController::class, "login"])
+    ->middleware("guest")
+    ->name("api-login");
+Route::post("logout", [LogoutController::class, "logout"])->name("api-logout");
 
-Route::middleware('ensureStaffEmailProvided')->post(
-    'register',
-    [RegisterController::class, 'register']
-);
+Route::middleware(["guest", EnsureStaffEmailProvided::class])
+    ->post("register", [RegisterController::class, "register"])
+    ->name("api-register");
 
-Route::get('email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->name('verification.verify');
+Route::get("email/verify/{id}/{hash}", [
+    VerificationController::class,
+    "verify",
+])->name("verification.verify");
