854b3e26842d8fba355cd8e98a8609964e94f365
Mouayad <muayad.perun@outlook.com>
2022-09-08 16:00:29 +0300

    add names to userRoutes

--- routes/api/userRoutes.php
+++ routes/api/userRoutes.php
@@ -1,12 +1,12 @@
 <?php
 
+namespace Routes\Api;
+
+use App\Http\Controllers\Api\User\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Admin\UserController;
-use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 
-
-Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
-
-    Route::get('me', [UserController::class, 'me']);
-    Route::delete('users/{user}', [UserController::class, 'destroy']);
+Route::group(["middleware" => ["auth:sanctum", "verified"]], function () {
+    Route::get("me", [UserController::class, "show"])->name("get-my-info");
+    Route::delete("users/{user}", [UserController::class, "destroy"])
+        ->name("delete-my-account");
 });
