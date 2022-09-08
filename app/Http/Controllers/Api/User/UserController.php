9922820c92b49b8d77c475314659648d77ada969
Mouayad <muayad.perun@outlook.com>
2022-09-08 15:53:56 +0300

    move UserController to Api\User folder

---
+++ app/Http/Controllers/Api/User/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\Role;
+use App\Models\User;
+use App\Services\UserService;
+use App\Traits\PaginatesResources;
+use App\Traits\ProvidesApiJsonResponse;
+use App\Traits\ProvidesResourcesJsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    use ProvidesResourcesJsonResponse,
+        PaginatesResources,
+        ProvidesApiJsonResponse;
+
+    public function __construct()
+    {
+        $this->middleware(["auth:sanctum", "verified"]);
+        $this->setResource(UserResource::class);
+        $this->setPerPage(env("USER_PER_PAGE"));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResource|null
+     **/
+    public function index(): ?JsonResource
+    {
+        return $this->collection($this->paginateWhenNeeded(User::all()));
+    }
+
+    /**
+     * Get staff users only.
+     *
+     * @return JsonResource|null
+     **/
+    public function getStaffUsers(): ?JsonResource
+    {
+        $patient_role_id = Role::getIdBySlug("patient");
+        return $this->collection(
+            $this->paginateWhenNeeded(
+                User::whereNot("role_id", $patient_role_id)
+            )
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(
+        Request $request,
+        User $user,
+        UserService $userService
+    ) {
+        $deleted = $userService->delete($user, $request->user());
+        if ($deleted) {
+            return $this->successResponse(
+                status_code: Response::HTTP_NO_CONTENT
+            );
+        } else {
+            return $this->errorResponse([
+                "message" =>
+                    "user with id (" . $user->id . ") was not deleted!",
+            ]);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResource|null
+     */
+    public function show(Request $request): ?JsonResource
+    {
+        return $this->resource($request->user());
+    }
+}
