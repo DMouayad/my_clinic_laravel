0e73536822024043c6fb30c809e58094a105e566
Mouayad <muayad.perun@outlook.com>
2022-09-08 16:04:27 +0300

    update EnsureStaffEmailProvided middleware returned error status code

--- app/Http/Middleware/EnsureStaffEmailProvided.php
+++ app/Http/Middleware/EnsureStaffEmailProvided.php
@@ -3,26 +3,27 @@
 namespace App\Http\Middleware;
 
 use App\Models\StaffEmail;
-use Closure;
-use Illuminate\Http\Request;
 use App\Traits\ProvidesApiJsonResponse;
+use Closure;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class EnsureStaffEmailProvided
 {
     use ProvidesApiJsonResponse;
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
         $email_to_register = $request->validate(['email' => 'required|email'])['email'];
 
-        $staff_email =  StaffEmail::where('email', $email_to_register)->first();
+        $staff_email = StaffEmail::where('email', $email_to_register)->first();
 
         if ($staff_email) {
             return $next($request);
@@ -31,7 +32,7 @@ class EnsureStaffEmailProvided
                 [
                     'message' => 'The email address (' . $email_to_register . ') is not allowed to register.',
                 ],
-                JsonResponse::HTTP_UNAUTHORIZED,
+                JsonResponse::HTTP_FORBIDDEN,
             );
         }
     }
