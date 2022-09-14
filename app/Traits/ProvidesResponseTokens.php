6f794c75b53bef6c5265e1ca3f67054313afe0ef
Mouayad <muayad.perun@outlook.com>
2022-09-14 09:14:47 +0300

    add trait ProvidesResponseTokens.php
    update ProvidesResponseTokens.php - add accessToken expires_at

---
+++ app/Traits/ProvidesResponseTokens.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Traits;
+
+use Carbon\Carbon;
+use DDragon\SanctumRefreshToken\NewRefreshToken;
+use Laravel\Sanctum\NewAccessToken;
+
+trait ProvidesResponseTokens
+{
+    /**
+     * @param NewRefreshToken $refreshToken
+     * @param NewAccessToken $accessToken
+     * @return array
+     */
+    public function getResponseTokens(
+        NewRefreshToken $refreshToken,
+        NewAccessToken  $accessToken
+    ): array
+    {
+        // get access token expires_at if it was specified for this token
+        // or else get default expiration for sanctum PersonalAccessToken
+        $accessTokenExpiresAt =
+            $accessToken->accessToken->expires_at ??
+            config("sanctum.expiration")
+                ? Carbon::now()->addMinutes(config("sanctum.expiration"))
+                : null;
+
+        return [
+            "access_token" => [
+                "token" => $accessToken->plainTextToken,
+                "expires_at" => $accessTokenExpiresAt,
+            ],
+            "refresh_token" => $refreshToken->plainTextToken,
+        ];
+    }
+}
