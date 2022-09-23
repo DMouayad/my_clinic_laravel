b4ce24cfa46dc73f297590dc96a9123305702938
Mouayad <muayad.perun@outlook.com>
2022-09-23 17:42:51 +0300

    add docs for ProvidesCustomRequestValidator

--- app/Traits/ProvidesCustomRequestValidator.php
+++ app/Traits/ProvidesCustomRequestValidator.php
@@ -8,7 +8,11 @@ use Illuminate\Validation\ValidationException;
 
 trait ProvidesCustomRequestValidator
 {
-    /**
+    /** Validates that request includes all specified rules
+     *
+     *  uses http request validator but Throws a customValidationException
+     * in case of an invalid or a missing required parameter.
+     *
      * @param \Illuminate\Http\Request $request
      * @param array $rules
      * @return array
