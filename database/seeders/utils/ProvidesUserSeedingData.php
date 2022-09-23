c2de99243009d90cc47d58eead6b046aa80019b0
Mouayad <muayad.perun@outlook.com>
2022-09-23 17:52:54 +0300

    update getRandomPhoneNum to return string instead of int

--- database/seeders/utils/ProvidesUserSeedingData.php
+++ database/seeders/utils/ProvidesUserSeedingData.php
@@ -2,20 +2,17 @@
 
 namespace Database\Seeders\Utils;
 
-
-use Illuminate\Support\Str;
-
 trait ProvidesUserSeedingData
 {
-    public $default_password = 'clinic123';
+    public $default_password = "clinic123";
     public $users_seeding_emails = [
-        'admin' => 'admin@myclinic.com',
-        'dentist' => 'dentist@myclinic.com',
-        'secretary' => 'secretary@myclinic.com',
+        "admin" => "admin@myclinic.com",
+        "dentist" => "dentist@myclinic.com",
+        "secretary" => "secretary@myclinic.com",
     ];
 
     public function getRandomPhoneNum()
     {
-        return Str::random(9);
+        return str(random_int(min: 111111111, max: 999999999));
     }
 }
