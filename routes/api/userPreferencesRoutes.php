524e98ebf7c839986c11e3d1792a1494af2fec0b
Mouayad <muayad.perun@outlook.com>
2022-09-24 13:41:27 +0300

    update userPreferencesRoutes.php

--- routes/api/userPreferencesRoutes.php
+++ routes/api/userPreferencesRoutes.php
@@ -5,9 +5,10 @@ namespace Routes\Api;
 use App\Http\Controllers\Api\User\UserPreferencesController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(["auth:sanctum", "verified"])->group(function () {
-    Route::apiResource(
-        "userPreferences",
-        UserPreferencesController::class
-    )->except(["index"]);
-});
+Route::middleware(["auth:sanctum", "verified"])
+    ->group(function () {
+        Route::get('me/preferences', [UserPreferencesController::class, 'show']);
+        Route::post('me/preferences', [UserPreferencesController::class, 'store']);
+        Route::put('me/preferences', [UserPreferencesController::class, 'update']);
+        Route::delete('me/preferences', [UserPreferencesController::class, 'destroy']);
+    });
