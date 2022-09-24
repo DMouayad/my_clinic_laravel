02d7d97f5a8f3463211b4cfe8a6c13d4bf97859c
Mouayad <muayad.perun@outlook.com>
2022-09-24 18:06:53 +0300

    remove unnecessary var_dump

--- tests/Feature/Api/Admin/Users/GetAllUsersTest.php
+++ tests/Feature/Api/Admin/Users/GetAllUsersTest.php
@@ -20,7 +20,7 @@ class GetAllUsersTest extends BaseUserApiRequestTest
     function test_authorized_request_returns_success_response()
     {
         $response = $this->makeRequestAuthorizedByUser("admin");
-        var_dump($response->content());
+
         $response->assertStatus(Response::HTTP_OK)->assertJson(
             fn(AssertableJson $json) => $json
                 ->has("data", config("my_clinic.seeded_users_count"))
