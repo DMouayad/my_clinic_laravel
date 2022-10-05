707cafa5b3fb411ff2d44d87bca43b9665c15cf1
Mouayad <muayad.perun@outlook.com>
2022-10-05 14:34:10 +0300

    use UserPreferencesResource in UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,7 +20,7 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'phone_number' => $this->phone_number,
             'role' => $this->whenLoaded('role'),
-            'preferences' => $this->whenLoaded('preferences'),
+            'preferences' => new  UserPreferencesResource($this->whenLoaded('preferences')),
             'email_verified_at' => $this->email_verified_at,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
