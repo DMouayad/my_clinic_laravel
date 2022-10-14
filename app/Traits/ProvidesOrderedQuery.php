e271e34bb22979fe7e398d9f91ad2def3550a09f
Mouayad <muayad.perun@outlook.com>
2022-10-14 16:49:45 +0300

    update ProvidesOrderedQuery

--- app/Traits/ProvidesOrderedQuery.php
+++ app/Traits/ProvidesOrderedQuery.php
@@ -2,25 +2,36 @@
 
 namespace App\Traits;
 
-
-use Illuminate\Support\Str;
-
 trait ProvidesOrderedQuery
 {
     /**
      * @param \Illuminate\Database\Eloquent\Model::query $query
      * @param string|null $sortBy
-     * @return \Illuminate\Database\Eloquent\Model::query
+     * @param array $allowed sorting will be done only by items in this array
+     * @param array $ignored
+     * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function queryWithSort($query, ?string $sortBy)
-    {
-        if ($sortBy) {
-            $sort_attributes =  explode(',', $sortBy);
+    public function orderQueryFromUrlParam(
+        $query,
+        ?string $sortBy,
+        array $allowed,
+        array $ignored = []
+    ) {
+        if (!empty($sortBy)) {
+            $sortBy = explode(",", $sortBy);
 
-            foreach ($sort_attributes as $sort) {
-                $order_direction = $sort[0] == '-' ? 'desc' : 'asc';
-                $sort = Str::remove(['-', '+'], $sort);
-                $query->orderBy($sort, $order_direction);
+            foreach ($sortBy as $sort_param) {
+                $sort_info = explode(" ", $sort_param);
+                $attribute = $sort_info[0];
+
+                // check if its allowed to sort by this attribute and its not from
+                // the ignored attributes list
+                if (
+                    in_array($attribute, $allowed) &&
+                    !in_array($attribute, $ignored)
+                ) {
+                    $query->orderBy($sort_info[0], $sort_info[1]);
+                }
             }
         }
         return $query;
