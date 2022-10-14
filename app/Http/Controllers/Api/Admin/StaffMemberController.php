00b4755689047d0da048da7b6f2f61c2b73deea7
Mouayad <muayad.perun@outlook.com>
2022-10-14 16:51:01 +0300

    update StaffMemberController - add custom sorting

--- app/Http/Controllers/Api/Admin/StaffMemberController.php
+++ app/Http/Controllers/Api/Admin/StaffMemberController.php
@@ -34,9 +34,81 @@ class StaffMemberController extends Controller
      */
     public function getStaffMembersWithRoles(Request $request): ?JsonResource
     {
-        return $this->collectionOfRequestQuery($request, StaffMember::query(), [
-            "role",
-        ]);
+        return $this->getStaffMemberCollection($request, ["role"]);
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param array $relations
+     * @return \Illuminate\Http\Resources\Json\JsonResource|null
+     */
+    private function getStaffMemberCollection(
+        Request $request,
+        array $relations = []
+    ): ?JsonResource {
+        $custom_sorting_fields = ["username", "role", "registered_with_at"];
+        $query = StaffMember::with($relations);
+
+        if ($request->sort) {
+            $sorting_allowed_attributes = [
+                "email",
+                "created_at",
+                ...$custom_sorting_fields,
+            ];
+            $sort_params = explode(",", $request->sort);
+            $sort_fields = [];
+            $sort_fields_direction = [];
+            array_walk($sort_params, function ($item) use (
+                &$sort_fields,
+                &$sort_fields_direction,
+                $sorting_allowed_attributes
+            ) {
+                $info = explode(" ", $item);
+                if (in_array($info[0], $sorting_allowed_attributes)) {
+                    $sort_fields[] = $info[0];
+                    $sort_fields_direction[$info[0]] = strtolower($info[1]);
+                }
+            });
+
+            // First, sort by StaffMember attributes
+            /**
+             * @var \Illuminate\Database\Eloquent\Builder $query
+             */
+            $query = $this->orderQueryFromUrlParam(
+                $query,
+                $request->sort,
+                allowed: $sorting_allowed_attributes,
+                ignored: $custom_sorting_fields
+            );
+
+            if (in_array("username", $sort_fields)) {
+                $is_descending = $sort_fields_direction["username"] == "desc";
+
+                $query->orderByRaw(
+                    "(select name from users where staff_members.user_id = users.id) " .
+                        ($is_descending ? "desc" : "is null")
+                );
+            }
+            if (in_array("registered_with_at", $sort_fields)) {
+                $is_descending =
+                    $sort_fields_direction["registered_with_at"] == "desc";
+
+                $query->orderByRaw(
+                    ($is_descending ? "" : "-") .
+                        "(select created_at from users where staff_members.user_id = users.id) desc"
+                );
+            }
+            if (in_array("role", $sort_fields)) {
+                $query->orderByRaw(
+                    "(select slug from roles where staff_members.role_id = roles.id) " .
+                        $sort_fields_direction["role"]
+                );
+            }
+        }
+        return $this->paginatedCollection(
+            $query,
+            per_page: $request->page ? null : 10000
+        );
     }
 
     /**
@@ -45,10 +117,7 @@ class StaffMemberController extends Controller
      */
     public function getStaffWithUsersAndRoles(Request $request): ?JsonResource
     {
-        return $this->collectionOfRequestQuery($request, StaffMember::query(), [
-            "role",
-            "user",
-        ]);
+        return $this->getStaffMemberCollection($request, ["role", "user"]);
     }
 
     /**
@@ -60,7 +129,7 @@ class StaffMemberController extends Controller
      */
     public function getStaffMembers(Request $request): JsonResource
     {
-        return $this->collectionOfRequestQuery($request, StaffMember::query());
+        return $this->getStaffMemberCollection($request);
     }
 
     /**
@@ -111,11 +180,10 @@ class StaffMemberController extends Controller
      * @throws \App\Exceptions\UserDoesntMatchHisStaffMemberException
      */
     public function update(
-        Request     $request,
+        Request $request,
         StaffMember $staff_member,
         UserService $userService
-    )
-    {
+    ) {
         $validated = $this->customValidate($request, [
             "email" => "nullable|email",
             "role" => "nullable|string",
@@ -157,11 +225,10 @@ class StaffMemberController extends Controller
      * @throws \App\Exceptions\UnauthorizedToDeleteUserException
      */
     public function destroy(
-        Request     $request,
+        Request $request,
         StaffMember $staff_member,
         UserService $userService
-    ): JsonResponse
-    {
+    ): JsonResponse {
         if ($staff_member->user) {
             $userService->delete($staff_member->user, $request->user());
         }
