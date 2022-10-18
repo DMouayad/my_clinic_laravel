d5a755af588f13f6d46093e6e1df410239e9102e
Mouayad <muayad.perun@outlook.com>
2022-10-18 18:27:19 +0300

    remove unique constraints of "name" column in "referesh_token_table"

--- packages/ddragon/sanctum-refresh-token/database/migrations/2019_12_14_000001_create_refresh_tokens_table.php
+++ packages/ddragon/sanctum-refresh-token/database/migrations/2019_12_14_000001_create_refresh_tokens_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create("refresh_tokens", function (Blueprint $table) {
             $table->id();
             $table->morphs("tokenable");
-            $table->string("name")->unique();
+            $table->string("name");
             $table->string("token", 64)->unique();
             $table->timestamp("expires_at")->nullable();
             $table->timestamps();
